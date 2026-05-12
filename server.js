a99ac118d4dd159ea011fc87e7c912fec43af06d
Segundo Alvarez <segundoav@hotmail.com>
2026-05-12 03:00:37 -0500

    Fix : Implement dynamic footer year add global CSS styles

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const NODE_ENV = "production";
+const NODE_ENV =  process.env.NODE_ENV || "development";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
